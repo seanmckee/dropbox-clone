7a54282ddc31a96e67b8b7bc4142b741ec6b6641
seanmckee <seanmingmckee@gmail.com>
2023-12-13 19:49:09 -0800

    authentication with clerk

---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import { SignInButton, SignedOut } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+      <Link className="flex items-center space-x-2" href="/">
+        <div className="bg-[#0160fe] w-fit">
+          <Image
+            src="https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png"
+            alt="logo"
+            className="invert"
+            height={50}
+            width={50}
+          />
+        </div>
+        <h1 className="font-bold text-xl">Dropbox</h1>
+      </Link>
+
+      <div className="px-5 flex space-x-2 items-center">
+        {/* theme toggler */}
+
+        <UserButton afterSignOutUrl="/" />
+        <SignedOut>
+          <SignInButton afterSignInUrl="/dashboard" mode="modal" />
+        </SignedOut>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
