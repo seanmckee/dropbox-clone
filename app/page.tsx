53a2441db032628ee6f6916e64cfafd92e6dae32
seanmckee <seanmingmckee@gmail.com>
2023-12-13 20:59:39 -0800

    homepage created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="">
-      <h1>Dropbox Clone</h1>
+      <div className="flex flex-col lg:flex-row items-center bg-[#1e1919] dark:bg-slate-800">
+        <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">
+            Welcome to Dropbox. <br />
+            <br />
+            Storing everything for you and your business needs. All in one place
+          </h1>
+          <p>
+            Enhance your personal storage with Dropbox, offering a simple and
+            efficient way to upload, organize, and access files from anywhere.
+            Securely store important documents and media, and experience the
+            convenience of easy file management and sharing in one centrailized
+            solution.
+          </p>
+
+          <Link href="/dashboard" className="flex bg-blue-500 p-5 w-fit">
+            Try it for free!
+            <ArrowRightIcon className="ml-10" />
+          </Link>
+        </div>
+        <div className="bg-[#1e1919] dark:bg-slate-800 h-full p-10">
+          <video autoPlay loop muted className="rounded-lg">
+            <source
+              src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
     </main>
   );
 }
