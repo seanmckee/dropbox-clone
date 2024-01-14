4f61f1a103d4b49e4bf511bd41d01338b8bf30e4
seanmckee <seanmingmckee@gmail.com>
2024-01-13 19:12:14 -0800

    delete file

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "@/firebase";
+import { Skeleton } from "../ui/skeleton";
 
 function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
   const { user } = useUser();
@@ -40,14 +41,40 @@ function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
 
   if (docs?.docs.length === undefined)
     return (
-      <div>
-        <p>Loading...</p>
+      <div className="flex flex-col">
+        <Button variant={"outline"} className="ml-auto w-36 h-10 mb-5">
+          <Skeleton className="h-5 w-full" />
+        </Button>
+
+        <div className="border rounded-lg">
+          <div className="border-b h-12" />
+          {skeletonFiles.map((file) => (
+            <div
+              key={file.id}
+              className="flex items-center space-x-4 p-5 w-full"
+            >
+              <Skeleton className="h-12 w-12" />
+              <Skeleton className="h-12 w-full" />
+            </div>
+          ))}
+
+          {skeletonFiles.length === 0 && (
+            <div className="flex items-center space-x-4 p-5 w-full">
+              <Skeleton className="h-12 w-12" />
+              <Skeleton className="h-12 w-full" />
+            </div>
+          )}
+        </div>
       </div>
     );
 
   return (
-    <div>
-      <Button onClick={() => setSort(sort === "desc" ? "asc" : "desc")}>
+    <div className="flex flex-col space-y-5 pb-10">
+      <Button
+        variant={"outline"}
+        className="ml-auto w-fit"
+        onClick={() => setSort(sort === "desc" ? "asc" : "desc")}
+      >
         Sort By {sort === "desc" ? "Newest" : "Oldest"}
       </Button>
       <DataTable columns={columns} data={initialFiles} />
